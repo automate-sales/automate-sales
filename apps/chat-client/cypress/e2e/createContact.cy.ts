faba065577e2d308e143a2c8c2ced42fa84ce0b1
gkpty <gkardonski@gmail.com>
2023-12-19 02:34:44 -0500

    adds template for missing tests

---
+++ apps/chat-client/cypress/e2e/createContact.cy.ts
@@ -0,0 +1,7 @@
+describe('test that new contacts get created when they send a message', () => {
+    it('should should display new message at the top of the sidebar', () => {
+        // new message should have an exclamation mark as it hasnt been seen/ answered
+        // click on the contact that sent you the message
+        // should take you to contact page and get the Id from the URL
+    })
+})
